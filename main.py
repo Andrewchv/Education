38116b6ad304992a7fc39da9afc144f251cf6f8e
Andrewchv <143516241+Andrewchv@users.noreply.github.com>
2023-11-07 10:28:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,82 +1,58 @@
-from pathlib import Path
-import shutil
-import sys
-import file_parser
-from normalize import normalize
+from datetime import date, timedelta
 
-def handle_media(file_name: Path, target_folder: Path):
-    target_folder.mkdir(exist_ok=True, parents=True)
-    file_name.replace(target_folder / normalize(file_name.name))
+def get_birthdays_per_week(users):
+    if not users:
+        return {}
 
-def handle_archive(file_name: Path, target_folder: Path):
-    target_folder.mkdir(exist_ok=True, parents=True)
-    folder_for_file = target_folder / normalize(file_name.name.replace(file_name.suffix, ''))
-    folder_for_file.mkdir(exist_ok=True, parents=True)
-    try:
-        shutil.unpack_archive(str(file_name.absolute()), str(folder_for_file.absolute()))
-    except shutil.ReadError:
-        folder_for_file.rmdir()
-        return
-    file_name.unlink()
+    # Знайти поточну дату та поточний тиждень
+    today = date.today()
+    current_day_of_week = today.weekday()
+    last_day_of_current_week = today + timedelta(days=7)
+
+    # Розділити користувачів за днями тижня, включаючи тих, чиї дні народження вже минули у цьому році
+    new_users = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
+
+    for user in users:
+        name = user['name']
+        birthday = user['birthday'].replace(year=today.year)
+        if birthday < today:  # якщо найближчий день народження менше поточної дати...
+            birthday = birthday.replace(year=today.year + 1)
+        # Перевірити, чи день народження випадає на поточний тиждень, включаючи тих, чиї дні народження вже минули у цьому році
+        if today <= birthday <= last_day_of_current_week:
+            # Якщо день народження припадає на вихідний (суботу або неділю), переносимо його на наступний понеділок
+
+            weekday = birthday.weekday()
+            if weekday == 0:
+                new_users['Monday'].append(name)
+            elif weekday == 1:
+                new_users['Tuesday'].append(name)
+            elif weekday == 2:
+                new_users['Wednesday'].append(name)
+            elif weekday == 3:
+                new_users['Thursday'].append(name)
+            elif weekday == 4:
+                new_users['Friday'].append(name)
+            elif weekday in (5, 6):
+                new_users['Monday'].append(name)
 
 
-def main(folder: Path):
-    file_parser.scan(folder)
-    for file in file_parser.JPEG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPEG')
-    for file in file_parser.JPG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPG')
-    for file in file_parser.PNG_IMAGES:
-        handle_media(file, folder / 'images' / 'PNG')
-    for file in file_parser.SVG_IMAGES:
-        handle_media(file, folder / 'images' / 'SVG')
-    for file in file_parser.MP3_AUDIO:
-        handle_media(file, folder / 'audio' / 'MP3')
-    for file in file_parser.OGG_AUDIO:
-        handle_media(file, folder / 'audio' / 'OGG')
-    for file in file_parser.WAV_AUDIO:
-        handle_media(file, folder / 'audio' / 'WAV')
-    for file in file_parser.AMR_AUDIO:
-        handle_media (file, folder / 'audio' / 'AMR')
-    for file in file_parser.AVI_VIDEO:
-        handle_media(file, folder / 'video' / 'AVI')
-    for file in file_parser.MP4_VIDEO:
-        handle_media(file, folder / 'video' / 'MP4')
-    for file in file_parser.MOV_VIDEO:
-        handle_media(file, folder / 'video' / 'MOV')
-    for file in file_parser.MKV_VIDEO:
-        handle_media(file, folder / 'video' / 'MKV')
-    for file in file_parser.DOC_DOCUMENT:
-        handle_media(file, folder / 'documents' / 'DOC')
-    for file in file_parser.DOCX_DOCUMENT:
-        handle_media(file, folder / 'documents' / 'DOCX')
-    for file in file_parser.TXT_DOCUMENT:
-        handle_media(file, folder / 'documents' / 'TXT')
-    for file in file_parser.PDF_DOCUMENT:
-        handle_media(file, folder / 'documents' / 'PDF')
-    for file in file_parser.XLSX_DOCUMENT:
-        handle_media(file, folder / 'documents' / 'XLSX')
-    for file in file_parser.PPTX_DOCUMENT:
-        handle_media(file, folder / 'documents' / 'PPTX')
-    for file in file_parser.ZIP_ARCHIVE:
-        handle_media(file, folder / 'archives' / 'ZIP')
-    for file in file_parser.GZ_ARCHIVE:
-        handle_media(file, folder / 'archives' / 'GZ')
-    for file in file_parser.TAR_ARCHIVE:
-        handle_media(file, folder / 'archives' / 'TAR')
-    for file in file_parser.MY_OTHER:
-        handle_media(file, folder / 'MY_OTHER')
-
-    for file in file_parser.ARCHIVES:
-        handle_archive(file, folder / 'ARCHIVES')
-
-    for folder in file_parser.FOLDERS[::-1]:
-        try:
-            folder.rmdir()
-        except OSError:
-            print(f'Error during remove folder {folder}')
-
+    # Видаляємо порожні списки зі словника
+    new_users = {day: names for day, names in new_users.items() if names}
+    print(new_users)
+    return new_users
 
 if __name__ == "__main__":
-    folder_process = Path(sys.argv[1])
-    main(folder_process.resolve())
+	# Тестові дані з користувачами
+	users = [
+		{'name': 'Masha', 'birthday': date ( 2021, 11, 11 )},
+		{'name': 'Olya', 'birthday': date ( 2023, 11, 12 )},
+		{'name': 'Kolya', 'birthday': date ( 1993, 11, 10 )},
+		# Додайте інших користувачів за потреби
+	]
+
+	result = get_birthdays_per_week ( users )
+	print ( result )
+
+	# Виводимо результат
+	for day_name, names in result.items ( ):
+		print ( f"{day_name}: {', '.join ( names )}" )
